428e022bd434d0a71cb9e9d9b2c32428b9257a36
Ricardo Montañana <rmontanana@gmail.com>
2023-06-03 17:32:23 +0200

    Chapter 5

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,5 +12,8 @@ app.jinja_env.auto_reload = True
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+# sets de default login view
+login.login_view = "login"
 
 from app import routes, models
